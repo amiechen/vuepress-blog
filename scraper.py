8c53454c72f3fb8b757a00b7bcb120ff7be81c6e
Amie Chen <chinghc@gmail.com>
2018-03-16 17:31:16 -0700

    write shortcuts to a yml file

--- scraper.py
+++ scraper.py
@@ -1,13 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
 
-shortcut_url = "https://helpx.adobe.com/illustrator/using/default-keyboard-shortcuts.html"
-
+shortcut_url = "https://helpx.adobe.com/photoshop/using/default-keyboard-shortcuts.html"
 page = requests.get(shortcut_url)
 soup = BeautifulSoup(page.text, 'html.parser')
-trs = soup.find_all('tr');
+sections = soup.select('.filter-content.section')
+yml = open('Photoshop.yml', 'w+')
 
-for row in trs:
-  row_array = row.find_all('td')
-  if len(row_array) == 3:
-    print ("{0}: {1}".format(row_array[0].getText().replace('\n', ' '), row_array[-1].getText().replace('\n', ' ')))
+for section in sections:
+  header = section.select('.header.first-header.header-top')
+  headerText = header[0].getText().strip().replace('\n', ' ').replace('\r', ' ')
+  trs = section.find_all('tr');
+
+  yml.write('{0}: \n'.format(headerText))
+  for row in trs:
+    row_array = row.find_all('td')
+    if len(row_array) == 3:
+      yml.write('  - "{0}": "{1}"\n'.format(row_array[0].getText().replace('\n', ''), row_array[-1].getText().replace('\n', '')))
