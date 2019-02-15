1d4e652a4b64db4e8718b08be5072722d6e01378
Amie Chen <chinghc@gmail.com>
2019-02-15 19:03:08 +0800

    add section for router-link

--- .vuepress/theme/components/Nav.vue
+++ .vuepress/theme/components/Nav.vue
@@ -3,16 +3,16 @@
     <nav
       class="font-sans bg-white text-center flex justify-between my-4 mx-auto container overflow-hidden"
     >
-      <a
-        href="/"
+      <router-link
+        :to="'/'"
         class="block text-left no-underline font-bold text-black uppercase"
-      >{{$site.title}}</a>
+      >{{$site.title}}</router-link>
       <ul class="text-sm text-grey-dark list-reset flex items-center">
         <li v-for="item in $site.themeConfig.nav">
-          <a
-            :href="item.link"
+          <router-link
+            :to="item.link"
             class="inline-block py-2 px-3 text-grey-darkest hover:text-grey-dark no-underline"
-          >{{item.text}}</a>
+          >{{item.text}}</router-link>
         </li>
       </ul>
     </nav>
